adf70d84af5c291257cd6f9f676a517697097bd4
pranjalsk <pkaranka@asu.edu>
2018-03-10 18:49:11 -0700

    local to git

--- java-client/src/com/client/java/SimpleClient.java
+++ java-client/src/com/client/java/SimpleClient.java
@@ -1,10 +1,16 @@
 package com.client.java;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class SimpleClient {
 
@@ -12,18 +18,27 @@ public class SimpleClient {
 
 		String url = "http://localhost:8080/topics";
 		
-		connectRemote(url);
+		try {
+			sendGet(url);
+			
+			sendPost(url);
+			
+			sendGet(url);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
 	
-	public static void connectRemote(String urlStr){
+	public static void sendGet(String urlStr) throws IOException, ParseException{
 		
-		try {
 			URL url = new URL(urlStr);
-			
+	
 			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-			
+						
 			conn.setRequestMethod("GET");
 			conn.setRequestProperty("Accept", "application/json");
 			
@@ -33,19 +48,75 @@ public class SimpleClient {
 			
 			InputStreamReader in = new InputStreamReader(conn.getInputStream());
 			BufferedReader br = new BufferedReader(in);
+			
 			String output;
+			StringBuilder jsonStr = new StringBuilder();
+			
 			while((output = br.readLine())!=null){
-				System.out.println(output);
+				jsonStr.append(output);
 			}
 			
 			conn.disconnect();
 	
-		} catch (Exception e) {
-			System.out.println("Exception in NetClientGet:- " + e);
-		}
-		
-		
+			JSONParser parser = new JSONParser();
+			System.out.println("JSON response: \n"+jsonStr.toString());
+			
+			Object obj = parser.parse(jsonStr.toString());
+			
+			JSONArray jsonArr = (JSONArray) obj;
+			
+			String id = (String) ((JSONObject)jsonArr.get(0)).get("id");
+			String name = (String) ((JSONObject)jsonArr.get(0)).get("name");
+			String description = (String) ((JSONObject)jsonArr.get(0)).get("description");
+
+			Topic topic = new Topic(id, name, description);
+			
+			System.out.println(topic.toString());
 	}
 	
+	
+	public static void sendPost(String urlStr) throws IOException{
+		
+		URL url = new URL(urlStr);
+		
+		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		con.setRequestProperty("Content-Type", "application/json");
+		
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		
+		JSONObject jsonObj = new JSONObject();
+        jsonObj.put("id", 3);
+        jsonObj.put("name", "MCS");
+        jsonObj.put("description", "MCS is core");
+
+		
+		wr.writeBytes(jsonObj.toJSONString());
+		wr.flush();
+		wr.close();
+		
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + jsonObj.toJSONString());
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+		
+	}
 
 }
